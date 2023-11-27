394acf3e8f28a0841ce773bb3437c85f99195f4b
ebrt433 <125595538+ebrt433@users.noreply.github.com>
2023-11-27 15:20:34 +0100

    Files pt.2

---
+++ problem.py
@@ -0,0 +1,37 @@
+import os
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import TimeSeriesSplit
+
+problem_title = "Bike count prediction"
+_target_column_name = "log_bike_count"
+# A type (class) which will be used to create wrapper objects for y_pred
+
+
+def get_cv(X, y, random_state=0):
+    cv = TimeSeriesSplit(n_splits=8)
+    rng = np.random.RandomState(random_state)
+
+    for train_idx, test_idx in cv.split(X):
+        # Take a random sampling on test_idx so it's that samples are not consecutives.
+        yield train_idx, rng.choice(test_idx, size=len(test_idx) // 3, replace=False)
+
+
+def _read_data(path, f_name):
+    data = pd.read_parquet(os.path.join(path, "data", f_name))
+    # Sort by date first, so that time based cross-validation would produce correct results
+    data = data.sort_values(["date", "counter_name"])
+    y_array = data[_target_column_name].values
+    X_df = data.drop([_target_column_name, "bike_count"], axis=1)
+    return X_df, y_array
+
+
+def get_train_data(path="."):
+    f_name = "train.parquet"
+    return _read_data(path, f_name)
+
+
+def get_test_data(path="."):
+    f_name = "test.parquet"
+    return _read_data(path, f_name)
